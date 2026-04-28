2fefaff91a9d81dc9761192b17a6429183b166d9
Gabriel <grizzly@Gabriels-MacBook-Air.local>
2026-04-28 05:09:59 -0400

    when clicking on movie popup appears and closes

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const movies = document.querySelectorAll('.movie');
+// stores all imgs in list
+const movies = document.querySelectorAll(".movie");
+//targets popup modal elements
+const modal = document.getElementById("movieModal");
+//get image in popup modal
+const modalImage = document.getElementById("modalImage");
+//get movie name in popup modal
+const modalTitle = document.getElementById("modalTitle");
+//get close button in popup modal
+const closeModal = document.getElementById("closeModal");
 
+// loops through every movie
 movies.forEach(function(movie) {
+    //when a movie is clicked, open the modal and set the image and title to match the clicked movie
     movie.addEventListener("click", function() {
-        console.log(movie.alt);
+        //make modal(popup) visible
+        modal.style.display = "flex";
+        // takes image and put inside popup
+        modalImage.src = movie.src;
+        //take alt movie name and display in popup
+        modalTitle.textContent = movie.alt;
     });
+});
+// when you click the close button, hide the modal
+closeModal.addEventListener("click", function() {
+    modal.style.display = "none";
 });
